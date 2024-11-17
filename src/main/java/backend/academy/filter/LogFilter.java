9e5067536c42528609f99b9c56596389f5ec66a8
shcherbanev <shcherbanev1@yandex.ru>
2024-11-17 12:24:47 +0300

    feat: create LogFilter

---
+++ src/main/java/backend/academy/filter/LogFilter.java
@@ -0,0 +1,65 @@
+package backend.academy.filter;
+
+import backend.academy.model.LogRecord;
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.function.Predicate;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogFilter implements Predicate<LogRecord> {
+
+    private final String fieldName;
+    private final String value;
+    private final String operator;
+
+    public LogFilter(String fieldName, String value, String operator) {
+        this.fieldName = fieldName;
+        this.value = value;
+        this.operator = operator;
+    }
+
+    @Override
+    public boolean test(LogRecord logRecord) {
+        try {
+            Object fieldValue = getFieldValue(logRecord, fieldName);
+            if (fieldValue == null) {
+                return false;
+            }
+
+            if ("=".equals(operator)) {
+                return fieldValue.toString().equals(value);
+            } else if (fieldValue instanceof LocalDateTime dateValue) {
+                LocalDate filterDate = LocalDate.parse(value);
+                return switch (operator) {
+                    case ">=" -> dateValue.isAfter(filterDate.atStartOfDay()) || dateValue.isEqual(
+                        filterDate.atStartOfDay());
+                    case "<=" -> dateValue.isBefore(filterDate.atStartOfDay()) || dateValue.isEqual(
+                        filterDate.atStartOfDay());
+                    default -> throw new IllegalArgumentException("Unsupported operator: " + operator);
+                };
+            }
+        } catch (IllegalArgumentException e) {
+            log.error(e.getMessage());
+        } catch (Exception e) {
+            log.error("Filter not applied. Invalid field or field value {} - {}", fieldName, value);
+        }
+        return false;
+    }
+
+    private Object getFieldValue(LogRecord logRecord, String fieldName) {
+        return switch (fieldName) {
+            case "method" -> logRecord.method();
+            case "from", "to" -> logRecord.localDate();
+            case "httpCode" -> logRecord.httpStatus().httpCode();
+            case "agent" -> logRecord.httpUserAgent();
+            case "remote-address" -> logRecord.remoteAddr();
+            case "remote-user" -> logRecord.remoteUser();
+            case "bytes" -> logRecord.bytes();
+            case "request" -> logRecord.request();
+            default -> throw new IllegalStateException("Unexpected value: " + fieldName);
+        };
+    }
+
+}
