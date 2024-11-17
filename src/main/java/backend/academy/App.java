3a9297be7500db38dae21f4927be65bfd40662ad
shcherbanev <shcherbanev1@yandex.ru>
2024-11-17 17:43:34 +0300

    feat: create App, Main class
    if applied allows run program from console

---
+++ src/main/java/backend/academy/App.java
@@ -0,0 +1,38 @@
+package backend.academy;
+
+import backend.academy.filter.LogFilter;
+import backend.academy.model.LogReport;
+import backend.academy.model.LogsStatistic;
+import backend.academy.service.LogService;
+import backend.academy.service.handler.LogHandler;
+import backend.academy.service.writer.ReportWriter;
+import backend.academy.util.Args;
+import backend.academy.util.InstanceFabric;
+import com.beust.jcommander.JCommander;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import static backend.academy.util.InstanceFabric.createFilters;
+import static backend.academy.util.InstanceFabric.createLogHandler;
+import static backend.academy.util.InstanceFabric.createReportWriter;
+
+@Slf4j
+public class App {
+
+    public void run(String[] args) {
+        Args arg = new Args();
+        if (!"analyzer".equals(args[0])) {
+            throw new RuntimeException("Invalid program mode");
+        }
+        JCommander.newBuilder().addObject(arg).build().parse(Arrays.copyOfRange(args, 1, args.length));
+
+        LogHandler logHandler = createLogHandler(arg.path());
+        LogFilter[] filters = createFilters(arg.filterField(), arg.filterValue(), arg.from(), arg.to());
+        LogService logService = new LogService();
+        LogsStatistic logsStatistic = new LogsStatistic();
+        logHandler.fetchLogs(arg.path(), logService, logsStatistic, filters);
+        LogReport logReport = logService.generateReport(logsStatistic);
+        ReportWriter reportWriter = createReportWriter(arg.format());
+        reportWriter.writeReport(logReport, InstanceFabric.generateReportFilePath(arg.path(), arg.format()));
+    }
+
+}
---
+++ src/main/java/backend/academy/util/InstanceFabric.java
@@ -0,0 +1,56 @@
+package backend.academy.util;
+
+import backend.academy.filter.LogFilter;
+import backend.academy.service.handler.FileLogHandler;
+import backend.academy.service.handler.HttpLogHandler;
+import backend.academy.service.handler.LogHandler;
+import backend.academy.service.writer.AdocWriter;
+import backend.academy.service.writer.MarkDownWriter;
+import backend.academy.service.writer.ReportWriter;
+import lombok.experimental.UtilityClass;
+import java.util.ArrayList;
+import java.util.List;
+
+@UtilityClass
+public class InstanceFabric {
+
+    private final static String ADOC_FORMAT = "adoc";
+    private final static String MARKDOWN_FORMAT = "md";
+
+    public static LogHandler createLogHandler(String path) {
+        if (path.contains("http")) {
+            return new HttpLogHandler();
+        }
+        return new FileLogHandler();
+    }
+
+    public static LogFilter[] createFilters(List<String> filterFields, List<String> filterValues, String from, String to) {
+        List<LogFilter> filters = new ArrayList<>();
+        if (filterFields != null && filterValues != null) {
+            for (int i = 0; i < filterFields.size(); i++) {
+                filters.add(new LogFilter(filterFields.get(i), filterValues.get(i), "="));
+            }
+        }
+        if (from != null) {
+            filters.add(new LogFilter("from", from, ">="));
+        }
+        if (to != null) {
+            filters.add(new LogFilter("to", to, "<="));
+        }
+        return filters.toArray(new LogFilter[0]);
+    }
+
+    public static ReportWriter createReportWriter(String format) {
+        if (ADOC_FORMAT.equals(format)) {
+            return new AdocWriter();
+        }
+        return new MarkDownWriter();
+    }
+
+    public static String generateReportFilePath(String filename, String format) {
+        return System.getProperty("user.dir") + "/report."
+            + (format == null ? MARKDOWN_FORMAT
+            : ADOC_FORMAT.equals(format) ? ADOC_FORMAT : MARKDOWN_FORMAT);
+    }
+
+}
