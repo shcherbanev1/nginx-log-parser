58951a5d8d6129aead894302cca6102bb8260932
shcherbanev <shcherbanev1@yandex.ru>
2024-11-17 17:42:04 +0300

    feat: create class for convenient argument parsing

---
+++ src/main/java/backend/academy/util/Args.java
@@ -0,0 +1,28 @@
+package backend.academy.util;
+
+import com.beust.jcommander.Parameter;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class Args {
+
+    @Parameter(names = {"--path", "-p"}, description = "Path to file with logs")
+    private String path;
+
+    @Parameter(names = {"--filter-field"})
+    private List<String> filterField;
+
+    @Parameter(names = {"--filter-value"})
+    private List<String> filterValue;
+
+    @Parameter(names = {"--format"})
+    private String format;
+
+    @Parameter(names = {"--from"})
+    private String from;
+
+    @Parameter(names = {"--to"})
+    private String to;
+
+}
